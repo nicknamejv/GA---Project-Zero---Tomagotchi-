b813c6fb2ee9f59f248c279b3c3d43fc5cb10943
nicknamejv <jonvercabral@yahoo.com>
2021-07-12 20:55:14 -0700

    Created User stories within my JS file and created milestones that will specifications of the project

---
+++ scripts/app.js
@@ -0,0 +1,82 @@
+/* SECTION: == Specifications ==
+
+Create a repo for your tomagotchi pet
+
+Make a commit after you finish each one of the following:
+
+Instatiate your Tomagotchi
+
+Display a character of your choice on the screen to represent your pet
+
+Display the following metrics for your pet:
+
+- Hunger (1-10 scale)
+- Sleepiness (1-10 scale)
+- Boredom (1-10 scale)
+- Age
+
+Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+
+/* NOTE: MORE DETAILS 
+- What is the increment of each aspect (both when you press X button and when it drains)?
+- For age how quickly will pet age?
+*/
+
+// Add the ability to name your pet.
+
+// Style the page.
+
+// Increase your pet's age every x minutes
+
+/* NOTE: MORE DETAILS 
+- will we keep the increment at minutes?
+- will we have a timer of some sort that will showcase in real time the age of the pet?
+- will we choose to evolve the pet at X minutes? 
+*/
+
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+
+/* NOTE: MORE DETAILS 
+- If the scale is between 1-10 which way would it flow? i.e. will it drain or will it gain?
+*/
+
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+
+/* NOTE: MORE DETAILS 
+- If pet hits 10 at on any of the properties and dies will we have a reset button? 
+- Will we reset the game or leave a prompt that states the pet has died?
+- Will this be a user click event or will it start after a set time?
+*/
+
+// Morph your pet at certain ages.
+
+/* NOTE: MORE DETAILS 
+- How many morphs will we have?
+- What age will trigger these morphs?
+- Will we give user the ability to morph the pet? 
+*/
+
+// Animate your pet across the screen while it's alive.
+
+/* NOTE: MORE DETAILS
+- In the most simplest way how will we choose to anime the pet?
+- Will it be moved around or stay in play?
+- Will hunger, boredom, or sleepiness showcase different pictures based on its level? 
+*/
+
+// SECTION: Feature Creep //
+// NOTE: This will list any features that come to mind along the way to be included after MVP
+
+
+// SECTION: Milestones // 
+
+// create the prompt that will ask user to input pet's name 
+// create our game object --> name, hunger, sleepiness, bordedom, age
+// create timer to showase age of pet --> when will the pet evolve? how will we increment timer?
+// create a event listener on button to start the game --> in this case it will hatch the egg and reveal the pet ? *maybe might take this idea out and just have a already hatched egg*
+// create the buttons that user will use in game (hunger, sleepiness, boredom, lights) --> what value will each button hold when user press? 
+// create a progress bar or someway to show case increase in hunger, sleepiness, and boredom (increase these bars at a set intervals)
+// create triggers for killing pet (when any of the properties hits 10 trigger death)
+// Morph the pet at X minutes
+// Animate the pet
+// Style the page
